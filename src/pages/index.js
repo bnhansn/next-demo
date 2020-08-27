55b7532955ba51b27e2bab412153f75bcc2dc4f6
Ben Hansen <bnhansn@gmail.com>
2020-08-26 19:48:29 -0700

    use persistent layout if channel rendered on subdomain

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import axios from 'axios'
+import Head from 'next/head'
 import ChannelVideos from '../components/ChannelVideos'
 
-export default function Home({ username, channel }) {
-  if (username && channel) {
-    return <ChannelVideos username={username} channel={channel} />
+export default function Home({ channel }) {
+  if (channel) {
+    return (
+      <Head>
+        <title>{channel.name}</title>
+      </Head>
+    )
   }
   return <h1>Welcome Home</h1>
+}
+
+Home.getLayout = (page) => {
+  const { channel, username } = page.props
+  if (channel && username) {
+    return <ChannelVideos {...page.props}>{page}</ChannelVideos>
+  }
+  return page
 }
 
 export async function getServerSideProps(context) {
